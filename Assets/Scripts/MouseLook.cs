4baeae84c9975db2b141f1e162c1a5dbd9866f22
Denis Yurkov <denisdeveloper136@gmail.com>
2020-10-23 19:31:51 +0200

    Доработал скрипт поворота камеры, сохранил модель в Blender для Леши

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -6,11 +6,19 @@ public class MouseLook : MonoBehaviour
 {
     [SerializeField] float mouseSpeed = 100f;
     [SerializeField] Transform player;
+    
+    float xRotation = 0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        /*
+         * Определяет, привязан ли аппаратный указатель к центру представления, 
+         * ограничен ли он окном или вообще не ограничен. 
+         * Когда заблокировано, курсор помещается в центр вида и не может быть перемещен.
+         * Состояние курсора может быть изменено операционной системой или Unity.
+         */
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
@@ -20,6 +28,14 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxis("Mouse X") * mouseSpeed * Time.deltaTime; 
         float mouseY = Input.GetAxis("Mouse Y") * mouseSpeed * Time.deltaTime;
 
+        xRotation -= mouseY;
+
+        /*// Используйте Clamp, чтобы ограничить значение диапазоном, который определяется минимальным и максимальным значениями.
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);*/
+
+        // localRotation - поворот относительно родительского элемента.
+        // Quaternion - Кватернионы используются для обозначения поворотов.
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         player.Rotate(Vector3.up * mouseX);
     }
 }
