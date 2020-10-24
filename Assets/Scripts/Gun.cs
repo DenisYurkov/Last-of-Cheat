bab0da530be078c4b576a07c662863a29472d6f1
Denis Yurkov <denisdeveloper136@gmail.com>
2020-10-24 14:07:01 +0200

    added shooting
    Wrote a firing script, added a sight, fixed a bug when moving a character...

---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    [SerializeField] float damage = 10f;
+    [SerializeField] float range = 100f;
+    [SerializeField] Camera fpsCam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        // Структура, используемая для получения информации из рейкаста.
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
