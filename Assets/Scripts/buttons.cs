048ccbc5ac064384b4e06d9089d6000ca9169d16
CoffeeLED <James.unevich@gmail.com>
2020-11-06 02:58:38 +0300

    Menu
    creation of main menu

---
+++ Assets/Scripts/buttons.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class buttons : MonoBehaviour
+{
+    public GameObject BgSet;
+    public GameObject BgMenu;
+
+    public void OnMouseUpAsButton() {
+         switch (gameObject.name)
+         {
+            case "Back":
+                BgMenu.SetActive(true);
+                BgSet.SetActive(false);
+                break;
+             case "Settings":
+                BgMenu.SetActive(false);
+                BgSet.SetActive(true);
+                Debug.Log("thhrhtrh");
+                 break;
+             case "Quit":
+                Application.Quit();
+                 Debug.Log("ghhtgt");
+                     break;
+         }
+
+    }
+}
